d4ed89253e797ef55ecd765d0d256a9844bd4a42
Shvejan <49836470+Shvejan@users.noreply.github.com>
2020-04-05 15:13:11 +0530

    allset4heroku

--- libs/mailgun.py
+++ libs/mailgun.py
@@ -4,7 +4,6 @@ def send_simple_message():
         "https://api.mailgun.net/v3/sandbox083a0db102e94e9daa60f4ddb416c558.mailgun.org/messages",
         auth=("api", "6e7ef9fe87f043b97a5feb8f5dd8d4f4-46ac6b00-71c037bd"),
         data={"from": "Excited User <mailgun@sandbox083a0db102e94e9daa60f4ddb416c558.mailgun.org>",
-              "to": ["shvejan2011@gmail.com"],
+              "to": ["example@gmail.com"],
               "subject": "Hello",
               "text": "Testing some Mailgun awesomness!"})
-
